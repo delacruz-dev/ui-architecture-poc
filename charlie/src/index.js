c5c64c3d24ad01924a8ad7804e22e393adc192b0
danderu <mail@dandel.es>
2016-09-16 15:14:27 +0200

    extract the business logic to a function

--- charlie/src/index.js
+++ charlie/src/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Alice from '@typeform/alice'
 import Bob from '@typeform/bob'
 
+const increment = (value) => value + 1
+
 export default class Charlie extends Component {
   constructor (...args) {
     super(...args)
@@ -14,7 +16,7 @@ export default class Charlie extends Component {
 
   handleButtonClick (counter) {
     this.setState({
-      counter: this.state.counter + 1
+      counter: increment(this.state.counter)
     })
   }
 
