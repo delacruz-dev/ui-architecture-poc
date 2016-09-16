0081151d307b1e262ddd8f33054fd28b56d1d40f
danderu <mail@dandel.es>
2016-09-16 09:50:58 +0200

    Bob is a component which renders inputs

--- bob/demo/components/App.js
+++ bob/demo/components/App.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import Bob from 'bob'
 
-export default () => <Bob />
+export default () => <Bob counter={3}/>
--- bob/src/index.js
+++ bob/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-export default () => (
-  <div>Bob</div>
+export default ({counter}) => (
+  <div>
+    <h1>Bob</h1>
+    {
+      Array.apply(null, {length: counter}).map(n => {
+        return (
+          <p>
+            <label>Input field:</label>
+            <input type='text' />
+          </p>
+        )
+      })
+    }
+  </div>
 )
