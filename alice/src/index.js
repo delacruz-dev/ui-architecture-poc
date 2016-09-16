3d7149cdf3767da4c065ed33776ced6edbdcd9de
danderu <mail@dandel.es>
2016-09-16 09:39:11 +0200

    Alice is a simple counter

--- alice/src/index.js
+++ alice/src/index.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default () => (
-  <div>Alice</div>
-)
+export default class Alice extends Component {
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      counter: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+  render () {
+    return (
+      <div>
+        <h1>Alice</h1>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    )
+  }
+}
